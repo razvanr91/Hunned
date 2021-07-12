4e1f807319e6921dbbea5b3100472cbf919eaf59
razvanr91 <razvanroman1991@gmail.com>
2021-07-12 15:39:41 +0100

    Fix Prism
    Fix Prism layout

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
-//Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+let btnSubmit = document.getElementById('btnSubmit');
+
+btnSubmit.addEventListener("click", getValues);
+
+// get the values from the UI
+function getValues() {
+    // get values from the page
+    let startValue = document.getElementById('startValue').value;
+    let endValue = document.getElementById('endValue').value;
+    alert(`The start value : `)
+    // we call generateNumbers
+    
+    // we call displayNumbers
+}
+
+// generate numbers from the start value to the end value
+function generateNumbers() {
+
+}
+
+// display numbers and  bold the even numbers
+function displayNumbers() {
+
 }
